6f5f07777d29fbb3e1d43d3e1a09de1be75805b0
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-07 08:29:22 +0200

    it shows me the latest data in postmam...

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -38,9 +38,10 @@ router.put('/:id', async (req, res) => {
         {
             name: req.body.name,
             icon: req.body.icon,
-            color: req.body.color
+            color: req.body.color,
 
-        }
+        },
+        { new: true }
     )
     if (!category)
         return res.status(404).send('The category cannot be created!')
