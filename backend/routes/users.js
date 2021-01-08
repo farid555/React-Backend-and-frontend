1447d024e53fc5357acc4445fa258c9b5eec0769
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-08 22:22:35 +0200

    Api add only name phone email...

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,7 +13,7 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const user = await User.findById(req.params.id).select('-passwordHash')
+    const user = await User.findById(req.params.id).select('name phone email')
     if (!user) {
         res.status(500).json({ success: 'The user with the given ID was not found. ' })
     }
