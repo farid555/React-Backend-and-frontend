614827d8b257c0f11bd5a03eca3fd8d981a463f9
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-08 06:59:22 +0200

    change cateagory from productList

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,7 +4,7 @@ const { Category } = require('../models/category')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find().select('')
+    const productList = await Product.find().populate('category')
     if (!productList) {
         res.status(500).json({ success: false })
     }
@@ -12,7 +12,7 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get(`/:id`, async (req, res) => {
-    const product = await Product.findById(req.params.id).populate('category')
+    const product = await Product.findById(req.params.id)
     if (!product) {
         res.status(500).json({ success: false })
     }
@@ -39,8 +39,35 @@ router.post('/', async (req, res) => {
 
     if (!product)
         return res.status(500).send('The product cannot be created')
-    return res.send(product)
+    res.send(product)
 })
+router.put('/:id', async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid Category')
 
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+
+        },
+        { new: true }
+    )
+    if (!category)
+        return res.status(500).send('The product cannot be updated!')
+
+    res.send(category)
+
+})
 
 module.exports = router;
