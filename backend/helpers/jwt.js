4d369733ab44362767d8434efe8fb1d4394a1da7
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-10 05:11:40 +0200

    permisson to users without login in products...

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,15 +2,16 @@ const expressJwt = require('express-jwt')
 
 function authJwt() {
     const secret = process.env.secret
+    const api = process.env.API_URL
     return expressJwt({
         secret,
         algorithms: ['HS256']
     }).unless({
         path: [
-            { url: '/api/v1/products', methods: ['GET', 'OPTIONS'] },
-
-            '/api/v1/users/login',
-            '/api/v1/users/register'
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
 
 
         ]
