58386b4b397b880c85426e6b0483ef9b2fc4a82c
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-11 10:50:24 +0200

    add user delete

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -91,6 +91,18 @@ router.get(`/get/count`, async (req, res) => {
         Count: userCount
     })
 })
+router.delete(`/:id`, async (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is deleted!' })// success deleted 
+        } else {
+            return res.status(404).json({ success: false, message: 'user not found!' })// find no found code
+        }
+
+    }).catch(err => {
+        return res.status(400).json({ success: false, error: err })//somekind of error wrong data or connection erros...
+    })
+})
 
 
 module.exports = router;
