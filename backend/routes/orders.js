11d7388b1c6f98c854def2834c0d39cdb540d4c4
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-13 18:38:15 +0200

    add orderitems quantity...

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -15,7 +15,9 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get(`/:id`, async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name')
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate('orderItems')
 
     if (!order) {
         res.status(500).json({ success: false })
